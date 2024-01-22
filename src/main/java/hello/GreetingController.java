08498de96c80dd767e4db16942bc23d65582870f
Radoslaw Drabczyk <radoslaw.drabczyk@dxc.com>
2024-01-22 06:37:21 +0100

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -14,7 +14,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World23") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World234") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
